e15f26a5c8ca93941e3dce7cd7cc691b63538b93
danid9 <152574903+danid9@users.noreply.github.com>
2025-02-27 19:30:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
  
-let Pswd = "trojan, tes1";
+let Pswd = "trojan";
 const proxyIPs = ["51.79.158.126:443"]; 
 let cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
